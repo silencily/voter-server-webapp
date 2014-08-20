ee5d9b21570149f83bc78a583dfc0fc90766b3a8
silencily <silencily@gmail.com>
2014-08-20 22:25:56 +0800

    增加mongodb数据库初始脚本

--- docs/db-schema/initdb.js
+++ docs/db-schema/initdb.js
@@ -9,12 +9,36 @@ function init() {
     db.user.insert({"username": "silencily", "email": "silencily@gmail.com",
         "password": "seven", "fullname": "silencily.seven", "smPhoto": "bytes",
         "lgPhoto": "bytes", "location": "Beijing", "joinedDate": new Date(),
-        "voteCounter": {"votes": 2, "voted": 3, "starred": 4}})
+        "voteCounter": {"votes": 2, "voted": 3, "starred": 4}});
 
     //init collection 'vote'
     db.vote.drop();
     db.createCollection("vote");
-    db.vote.insert({"title":"hello world",})
+    db.vote.insert({"title": "hello world", "creator": db.user.findOne()._id.valueOf(),
+        "createTime": new Date(), "voted": 2, "starred": 3, "isMulti": false, "isDel": false,
+        "delTime": null, "choices": [
+            {"no": 1, "content": "first choice", "voted": 2, "ratio": 0.2},
+            {"no": 2, "content": "second choice", "voted": 8, "ratio": 0.8}
+        ]});
+
+    //init collection 'notification'
+    db.notification.drop();
+    db.createCollection("notification");
+    db.notification.insert({"to": db.user.findOne()._id.valueOf(), "type": 0, "notiTime": new Date(),
+        "isReaded": false, "content": "hello world!", "isDel": false, "delTime": null})
+
+    //init collection 'voted'
+    db.voted.drop();
+    db.createCollection("voted");
+    db.voted.insert({"voter": db.user.findOne()._id.valueOf(),
+        "vote": db.vote.findOne()._id.valueOf(), "voteTime": new Date()});
+
+    //init collection 'starred'
+    db.starred.drop();
+    db.createCollection("starred");
+    db.starred.insert({"voter": db.user.findOne()._id.valueOf(),
+        "vote": db.vote.findOne()._id.valueOf(), "starredTime": new Date()});
 }
+
 init();
 
